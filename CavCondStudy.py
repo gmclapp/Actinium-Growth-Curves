659385a7098a374ba85ee24bf492705551f8fc25
Glenn Clapp <40251433+gmclapp@users.noreply.github.com>
2022-09-13 10:19:35 -0400

    Stats!

--- CavCondStudy.py
+++ CavCondStudy.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime as dt
+from scipy import stats
 
 def parse_dates(DF, date_col, time_col):
     new_series = []
@@ -24,6 +25,8 @@ def parse_date(date, time):
         print("Failed to parse time.",ex)
 
 def CavCondStudy(fig,ax):
+    timeframe = 43200
+    
     DF = pd.read_csv("output.csv")
     DFCond = pd.read_csv("Conditioning times.csv")
 
@@ -33,8 +36,62 @@ def CavCondStudy(fig,ax):
     ax.plot(DF["Date and Time"],DF["power"])
     for i, row in DFCond.iterrows():
         ax.vlines(row["Date and Time"],0,300,linestyles='dashed',color='k')
+
+    IterationB = []
+    PowerB = []
+    DTB = []
+
+    IterationA = []
+    PowerA = []
+    DTA = []
+    
+    for i, row in DFCond.iterrows():
+        for j, jow, in DF.iterrows():
+            if 0 > (jow["Date and Time"] - row["Date and Time"]).total_seconds() > -timeframe:
+                DTB.append(jow["Date and Time"].to_pydatetime())
+                PowerB.append(jow["power"])
+                IterationB.append(i)
+            elif 0 < (jow["Date and Time"] - row["Date and Time"]).total_seconds() < timeframe:
+                DTA.append(jow["Date and Time"].to_pydatetime())
+                PowerA.append(jow["power"])
+                IterationA.append(i)
+
+    Before = pd.DataFrame()
+    After = pd.DataFrame()
+    
+    Before["Date and Time"] = DTB
+    Before["power (W)"] = PowerB
+    Before["Iteration"] = IterationB
+
+    After["Date and Time"] = DTA
+    After["power (W)"] = PowerA
+    After["Iteration"] = IterationA
+
+    print(Before.head())
+
+    for i,row in Before.iterrows():
+        try:
+            ax.vlines(row["Date and Time"],0,300,colors='red',linestyles='dashed')
+        except:
+            continue
+
+    for i,row in After.iterrows():
+        try:
+            ax.vlines(row["Date and Time"],0,300,colors='green',linestyles='dashed')
+        except:
+            continue
+
+    paired_data =[]
+    for i in range(len(DFCond["Date and Time"])):
+        A = After.loc[After["Iteration"] == i]
+        B = Before.loc[Before["Iteration"] == i]
+        diff = A["power (W)"].mean() - B["power (W)"].mean()
         
-    print(DF["power"].head())
+        paired_data.append(diff)
+
+    tstat, pvalue = stats.ttest_1samp(paired_data,0)
+    print("test statistic: {:4.2f}\np-value: {:4.3f}".format(tstat,pvalue))
+    
     return(fig,ax)
 
 if __name__ == "__main__":
