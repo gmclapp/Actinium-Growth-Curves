2854d5aa84e5c947faa289b98bcafc3f65030198
Glenn Clapp <40251433+gmclapp@users.noreply.github.com>
2022-09-13 06:11:16 -0400

    Cavity Conditioning Study

---
+++ CavCondStudy.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def CavCondStudy(fig,ax):
+    return(fig,ax)
+
+if __name__ == "__main__":
+    fig,ax = plt.subplots(1,1,figsize=(10,6))
+    CavCondStudy()
